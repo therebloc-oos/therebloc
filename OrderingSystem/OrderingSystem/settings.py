6d1a34500bf6e5cb702d7fa64d934296f61d69cc
therebloc-oos <therebloc.oos@gmail.com>
2025-10-20 14:53:52 +0800

    Update settings.py

--- OrderingSystem/OrderingSystem/settings.py
+++ OrderingSystem/OrderingSystem/settings.py
@@ -173,9 +173,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'tupclaboratory@gmail.com')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'hyub dnjs etuf syjd')
-DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', 'tupclaboratory@gmail.com')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'therebloc.oos@gmail.com')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'yrgb xtoi upfu xhxn')
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', 'therebloc.oos@gmail.com')
 
 # Detect Render environment
 IS_RENDER = os.environ.get('RENDER', False)
