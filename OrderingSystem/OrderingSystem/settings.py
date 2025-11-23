1672601a06040b0ee2784fac63e210821ee0d3bd
therebloc-oos <therebloc.oos@gmail.com>
2025-11-23 21:19:55 +0800

    Update settings.py

--- OrderingSystem/OrderingSystem/settings.py
+++ OrderingSystem/OrderingSystem/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "insecure-fallback-key")
 
 DEBUG = os.environ.get("DEBUG", "False").lower() in ["true", "1"]
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "creativezone.onrender.com,creativezone.online,www.creativezone.online").split(",")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "therebloc.onrender.com,therebloc.online,www.therebloc.online").split(",")
 
 # CSRF Protection for HTTPS
 CSRF_TRUSTED_ORIGINS = [
